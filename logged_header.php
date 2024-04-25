93934393d877666a041c7f65c5cc75dbe55b060d
lorenzo7t <78906167+lorenzo7t@users.noreply.github.com>
2024-04-25 20:18:36 +0200

    a

---
+++ logged_header.php
@@ -0,0 +1,23 @@
+<div class="header fixed-width">
+    <div class="header-right">
+        <a href="index.php" class="logo-container">
+            <img class="logo" src="img/logo_new.png">
+        </a>
+        <div class="search-container">
+            <form action="search.php" method="GET">
+                <input type="text" name="search" id="search" class="search-bar" placeholder="Cerca...">
+                <button type="submit" class="search-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                    </svg>
+                </button>
+            </form>
+        </div>
+    </div>
+    <div class="header-left">
+        <a href="home.php" class="header-button">Home</a>
+        <a href="profile.php" class="header-button">Profilo</a>
+        <a href="logout.php" class="header-button">Logout</a>
+    </div>
+    </div>
