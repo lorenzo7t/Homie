ba730c631dbce2c0e10267f76d34eac3497bc989
lorenzo7t <78906167+lorenzo7t@users.noreply.github.com>
2024-04-25 22:05:40 +0200

    added professionisti section

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Landing</title>
+    <title>Homie - Homepage</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
