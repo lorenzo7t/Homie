167b0518e6d8fc9fbe9f2129df600a0167d6d0e1
lorenzo7t <78906167+lorenzo7t@users.noreply.github.com>
2024-05-14 15:25:56 +0200

    map

--- home-body.php
+++ home-body.php
@@ -2,37 +2,37 @@
     <div class="categories-container">
 
         <div class="category">
-            <button class="category-button">
+            <button class="category-button" data-category="idraulico">
                 <img src="img/idraulico.png" alt="idraulico">
             </button>
             <span class="category-name">Idraulico</span>
         </div>
         <div class="category">
-            <button class="category-button">
+            <button class="category-button" data-category="pittore">
                 <img src="img/pittore.png" alt="pittore">
             </button>
             <span class="category-name">Pittore</span>
         </div>
         <div class="category">
-            <button class="category-button">
+            <button class="category-button" data-category="elettricista">
                 <img src="img/elettricista.png" alt="elettricista">
             </button>
             <span class="category-name">Elettricista</span>
         </div>
         <div class="category">
-            <button class="category-button">
+            <button class="category-button" data-category="fabbro">
                 <img src="img/fabbro.png" alt="fabbro">
             </button>
             <span class="category-name">Fabbro</span>
         </div>
         <div class="category">
-            <button class="category-button">
+            <button class="category-button" data-category="colf">
                 <img src="img/colf.png" alt="colf">
             </button>
             <span class="category-name">Colf</span>
         </div>
         <div class="category">
-            <button class="category-button">
+            <button class="category-button" data-category="tuttofare">
                 <img src="img/tuttofare.png" alt="tuttofare">
             </button>
             <span class="category-name">Tuttofare</span>
@@ -44,16 +44,30 @@
         <div class="professionals-container">
             <div class="professionals-container-title">
                 <h1>Professionisti</h1>
+                <div class="favorites-toggle">
+                    <button class="favorites-toggle-button">
+                        <label class="favorites-toggle-container">
+                            <input type="checkbox">
+                            <svg id="Layer_1" version="1.0" viewBox="0 0 24 24" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <path d="M16.4,4C14.6,4,13,4.9,12,6.3C11,4.9,9.4,4,7.6,4C4.5,4,2,6.5,2,9.6C2,14,12,22,12,22s10-8,10-12.4C22,6.5,19.5,4,16.4,4z"></path>
+                            </svg>
+                        </label>
+                    </button>
+                </div>
+
             </div>
             <ul>
-                <li>
+                <!-- <li>
                     <div class="worker-entry" data-lat="41.9028" data-id="1" data-lng="12.4964" data-name="Mario Rossi" data-category="Idraulico" data-rating="4.5">
 
                         <div class="external-container">
                             <div class="img-infos-container">
-
-
-                                <img src="img/professionals/mario-rossi.jpeg" alt="mario-rossi">
+                                <div class="internal-container">
+                                    <img src="img/professionals/mario-rossi.jpeg" alt="mario-rossi">
+                                    <div class="favourite">
+                                        <?php include 'favorite_heart.php'; ?>
+                                    </div>
+                                </div>
                                 <div class="infos-container">
                                     <div class="worker-info">
                                         <span class="worker-name">Mario Rossi</span>
@@ -106,63 +120,7 @@
                             </div>
                         </div>
                     </div>
-                </li>
-
-                <li>
-                    <div class="worker-entry" data-lat="41.9050" data-id="2" data-lng="12.5000" data-name="Gianna Limone" data-category="Colf" data-rating="4.5">
-
-                        <div class="img-infos-container">
-
-
-                            <img src="img/professionals/gianna-limone.jpeg" alt="gianna-limone">
-                            <div class="infos-container">
-                                <div class="worker-info">
-                                    <span class="worker-name">Gianna Limone</span>
-                                    <span class="worker-category">Colf</span>
-
-                                    <div class="worker-price">
-                                        <div class="call-price">
-                                            <svg fill="#457B9D" width="800px" height="800px" viewBox="0 0 36 36" version="1.1" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                                <title>phone-handset-solid</title>
-                                                <path class="clr-i-solid clr-i-solid-path-1" d="M15.22,20.64a20.37,20.37,0,0,0,7.4,4.79l3.77-3a.67.67,0,0,1,.76,0l7,4.51a2,2,0,0,1,.33,3.18l-3.28,3.24a4,4,0,0,1-3.63,1.07,35.09,35.09,0,0,1-17.15-9A33.79,33.79,0,0,1,1.15,8.6a3.78,3.78,0,0,1,1.1-3.55l3.4-3.28a2,2,0,0,1,3.12.32L13.43,9a.63.63,0,0,1,0,.75l-3.07,3.69A19.75,19.75,0,0,0,15.22,20.64Z"></path>
-                                                <rect x="0" y="0" width="36" height="36" fill-opacity="0" />
-                                            </svg>
-                                            <span>€10</span>
-                                        </div>
-                                        <div class="separator-point">·</div>
-                                        <div class="hour-price">
-                                            <span>€12/ora</span>
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                                <div class="worker-rating">
-                                    <span class="rating">4.5</span>
-                                    <svg fill="#E63946" width="22px" height="22px" id="star" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
-                                        <path id="primary" d="M22,9.81a1,1,0,0,0-.83-.69l-5.7-.78L12.88,3.53a1,1,0,0,0-1.76,0L8.57,8.34l-5.7.78a1,1,0,0,0-.82.69,1,1,0,0,0,.28,1l4.09,3.73-1,5.24A1,1,0,0,0,6.88,20.9L12,18.38l5.12,2.52a1,1,0,0,0,.44.1,1,1,0,0,0,1-1.18l-1-5.24,4.09-3.73A1,1,0,0,0,22,9.81Z" style="fill: #E63946"></path>
-                                    </svg>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="worker-menu hidden">
-                            <div class="worker-menu-infos">
-                                <div class="worker-menu-info">
-                                    <div class="worker-position">
-                                        <span>Via dell'incenso, Roma</span>
-
-                                    </div>
-                                    <div class="is-active">
-                                    </div>
-                                </div>
-                            </div>
-                            <button class="worker-menu-button">Contatta</button>
-                        </div>
-                    </div>
-                </li>
-
-
-
+                </li> -->
             </ul>
 
         </div>
